d485d503b88a90564068d44f57a67ab348e826e8
kom3 <manjunathkankatteko@gmail.com>
2022-04-26 19:20:44 +0530

    initial project setup

---
+++ components/Home/home.jsx
@@ -0,0 +1,5 @@
+import styles from './home.module.scss'
+
+export default function Home() {
+  return <div className={styles.home_parent}>Home component</div>;
+}
---
+++ pages/index.jsx
@@ -0,0 +1,5 @@
+import Home from "../components/Home/home.jsx";
+
+export default function Index() {
+  return <Home />;
+}
